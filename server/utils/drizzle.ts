a6b01f10ccadb2d209687cf92cfc462946cc3aa5
bluesFee_codez <bluesfeecodez@gmail.com>
2025-09-05 18:05:00 +0200

    added drizzle

---
+++ server/utils/drizzle.ts
@@ -0,0 +1,12 @@
+import { drizzle } from "drizzle-orm/libsql";
+import * as schema from "../database/schema";
+import { createClient } from "@libsql/client";
+
+// You can specify any property from the libsql connection options
+export const useDrizzle = () => {
+  const client = createClient({ url: "file:db.sqlite" });
+  const db = drizzle({ client, schema: schema });
+  return db;
+};
+
+export type User = typeof schema.users.$inferSelect;
